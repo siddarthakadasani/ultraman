6c763e680ecd7ff31c7304ab7227b505f83bba6e
siddarthakadasani <kadasanisiddhartha.96@gmail.com>
2019-06-26 12:26:52 +0530

    initial commit

---
+++ ultraman.js
@@ -0,0 +1,12 @@
+function showCourses(){
+    console.log("browse courses");
+    document.getElementById('techCourseTwo').style.display = 'block';
+}
+function myFunction() {
+    var x = document.getElementById("");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
